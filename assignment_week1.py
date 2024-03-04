a1f77270576210f240d828fdfbbc47fa1e408e1b
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2024-03-04 15:03:59 +0545

    split method

--- assignment_week1.py
+++ assignment_week1.py
@@ -10,15 +10,24 @@ Enter your Name, DOB and Age. Calculate whether the correct age from the DOB
 
 
 
+# current_year = 2024
+# your_Name = str(input('Enter your name: '))
+# your_Age = int(input('Enter your age: '))
+# your_DOB = int(input('Enter your DOB: '))
+# calculated_age = current_year-your_DOB
+
+# if your_Age == calculated_age:
+#     print(f"{your_Name}, Your Age is correct!")
+
+# else :
+#     print ('Liar Liar pants on fire! Please enter the correct age!')
+
 current_year = 2024
-your_Name = str(input('Enter your name: '))
-your_Age = int(input('Enter your age: '))
-your_DOB = int(input('Enter your DOB: '))
-calculated_age = current_year-your_DOB
+name = input("Enter your name DOB and age:").split(' ')
+calc_age = current_year - int (name[-1].split('/')[0])
 
-if your_Age == calculated_age:
-    print(f"{your_Name}, Your Age is correct!")
+if (calc_age == int (name[-2])):
+    print(f'Hey {name[0]},your age is {calc_age}and you told us correct age.')
 
-else :
-    print ('Liar Liar pants on fire! Please enter the correct age!')
-
+else:
+     print(f'Hey {name[0]},your age is {calc_age}and you lied us about your age.')
