2fa8418f52c9dfb3cf2242309a6c8811622292e4
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2024-03-06 15:49:07 +0545

    list comprehensions and functions

--- day_4.py
+++ day_4.py
@@ -1,15 +1,15 @@
 '''
 if else conditions in python
 '''
-# marks = int(input('Enter your marks: '))
-# if (marks>=100):
-#     print("Distinction")
-# elif(marks>60 and marks<80):
-#     print("First division")
-# elif(marks>40 and marks<60):
-#     print('Pass')
-# else:
-#     print("Go for re-exam")
+marks = int(input('Enter your marks: '))
+if (marks>=100):
+    print("Distinction")
+elif(marks>60 and marks<80):
+    print("First division")
+elif(marks>40 and marks<60):
+    print('Pass')
+else:
+    print("Go for re-exam")
     
     
     
@@ -17,40 +17,40 @@ a = int(input('Enter your num a: '))
 b = int(input('Enter your num b: '))
 c = int(input('Enter your num c: '))
 
-# if (a>=b and a>=c):
-#     print(f'a {a} is greater!')
-#     if(b<c):
-#         print(f'b {b} is smallest!')
-#     else:
-#         print(f'c {c} is smallest!')
-# elif(b>=c):
-#     print(f'b {b} is greater!')
-#     if(a<c):
-#         print(f'a {a} is smallest!')
-#     else:
-#         print(f'c {c} is smallest')
+if (a>=b and a>=c):
+    print(f'a {a} is greater!')
+    if(b<c):
+        print(f'b {b} is smallest!')
+    else:
+        print(f'c {c} is smallest!')
+elif(b>=c):
+    print(f'b {b} is greater!')
+    if(a<c):
+        print(f'a {a} is smallest!')
+    else:
+        print(f'c {c} is smallest')
 
-# else:
-#   print(f'c {c} is greater!')
+else:
+  print(f'c {c} is greater!')
   
   
-# if (a>=b ):
-#     greatest = a 
-#     smallest = b
-#     if(b<c):
-#         smallest = b
-# elif(b>=c):
-#     greatest = b
-#     smallest = c 
-#     if(a<c):
-#        smallest = a 
-# else:
-#     greatest = c 
-#     smallest = b 
-#     if (a<b):
-#         smallest = a 
+if (a>=b ):
+    greatest = a 
+    smallest = b
+    if(b<c):
+        smallest = b
+elif(b>=c):
+    greatest = b
+    smallest = c 
+    if(a<c):
+       smallest = a 
+else:
+    greatest = c 
+    smallest = b 
+    if (a<b):
+        smallest = a 
 
-# print('smallest' + str(smallest) + 'largest'+str(greatest))
+print('smallest' + str(smallest) + 'largest'+str(greatest))
  
 
 n = int(input('Enter your num n: '))
--- day_5.py
+++ day_5.py
@@ -1,11 +1,11 @@
 '''
 loops
 '''
-# list_a = [x**2 for x in range(10)]
-# print(list_a)
+list_a = [x**2 for x in range(10)]
+print(list_a)
 
-# list_a = [x**2 for x in range(10)if x%2!=0]
-# print(list_a)
+list_a = [x**2 for x in range(10)if x%2!=0]
+print(list_a)
 
 '''
 list comprehension 
@@ -28,6 +28,12 @@ print(f"the most brilliant {', '.join(list_desc)} number is {userinput}")
 user_input = str(input("Enter any name:"))
 uppercase_letters = [char.upper() for char in user_input ]
 print(uppercase_letters)
+
+'''
+Functions in Python
+'''
+# Call, Arguement, parameter, return 
+
 
 
             
