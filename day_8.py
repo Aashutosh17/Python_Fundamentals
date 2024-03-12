1eed78b9a8186a011c55bac5bd9a1f0e13b91c65
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2024-03-12 16:27:01 +0545

    static method, dunder

--- day_8.py
+++ day_8.py
@@ -64,6 +64,46 @@ class Cat (Animal):
 dog1 = Dog("Jenny", 4, "Golden retriver")
 cat1 = Cat("Kiyo", 3, "Persian Cat")
 
-print(dog1.age)
-print(cat1.breed)
-print(cat1.makesounds())
+# print(dog1.age)
+# print(cat1.breed)
+# print(cat1.makesounds())
+
+
+class teacher():
+    salary=10000
+
+    def __init__(self,name,age) -> None:
+        self.name=name
+        self.age=age
+        self.email=name+str(age)+'@gmail.com'
+
+    def add_salary_a(self,per):
+        self.salary=self.salary *(1+(per/100))
+
+    @staticmethod
+    def print_S():
+        print("this is static method")
+        return "Sucessful"
+
+    @classmethod
+    def add_salary(cls,per):
+        cls.salary=cls.salary *(1+(per/100))
+    
+    def __add__(self,obj):
+        return self.age+obj.age
+
+t1=teacher('nabin',23)
+t2=teacher('ashutosh',25)
+
+
+print(t1.__dict__)
+print(t1.salary,t2.salary,teacher.salary)
+t1.salary=10100
+print(t1.__dict__)
+print(t1.salary,t2.salary,teacher.salary)
+teacher.add_salary(10)
+print(t1.salary,t2.salary,teacher.salary)
+t1.add_salary_a(10)
+print(t1.salary,t2.salary,teacher.salary)
+
+print(t1+ t2)
